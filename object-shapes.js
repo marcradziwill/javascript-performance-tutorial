6c513d8552f279675560a7d190d0bf127325f578
Marc Radziwill <hallo@marcradziwill.com>
2020-02-13 14:18:23 +0100

    changed object names

--- object-shapes.js
+++ object-shapes.js
@@ -17,10 +17,10 @@ let objectD = {
   z: 3
 };
 
-console.log("a is", objectA);
-console.log("b is", objectB);
-console.log("b is", objectC);
-console.log("b is", objectD);
+console.log("objectA is", objectA);
+console.log("objectB is", objectB);
+console.log("objectC is", objectC);
+console.log("objectD is", objectD);
 console.log("objectA and objectB have same shape:", %HaveSameMap(objectA, objectB));
 console.log("objectA and objectC have same shape:", %HaveSameMap(objectA, objectC));
 console.log("objectA and objectD have same shape:", %HaveSameMap(objectA, objectD));

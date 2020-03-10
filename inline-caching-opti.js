fa05af914c886100a0974a2d99ecb809fe714456
Marc Radziwill <hallo@marcradziwill.com>
2020-03-10 16:03:45 +0100

    added inline cachine

---
+++ inline-caching-opti.js
@@ -0,0 +1,18 @@
+function getX(o) {
+	return o.x;
+}
+const obj1 = {
+  x: 2,
+  y: 4.1
+}
+
+const obj2 = {
+  x: 4,
+  y: 2.2
+}
+
+let iterations = 1e7;
+while ( iterations-- ) {
+  getX(obj1);
+  getX(obj2);
+}
